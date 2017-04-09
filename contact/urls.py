3cd6e636ce4bd7dc141fb877a2151748d1737988
Adam Dobrawy <naczelnik@jawnosc.tk>
2017-04-09 17:15:57 +0200

    Update

--- contact/urls.py
+++ contact/urls.py
@@ -1,9 +1,8 @@
-from django.conf.urls import patterns, url
+from django.conf.urls import url
 from django.views.generic import TemplateView
 from . import views
 
-urlpatterns = patterns('',
+urlpatterns = [
     url(r'^$', views.ContactView.as_view(), name="form"),
     url(r'^/success$', TemplateView.as_view(template_name="contact/success.html"), name="success"),
-
-)
+]
